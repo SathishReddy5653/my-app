746a104d25f27282562b7512ae91ca7768ad3e6a
sathish <sathishreddy2902@gmail.com>
2023-08-28 18:54:34 +0530

    still need to change css

--- src/components/card-component/card.js
+++ src/components/card-component/card.js
@@ -1,32 +1,82 @@
 import React from "react";
 import "../card-component/card.css";
 import dataset from "../../images/dataset.png";
+import certified from "../../images/certified.jpg";
+import registered from '../../images/registered.jpeg'
+import pinned from '../../images/pin.png';
+import share from '../../images/share.png';
+import viewnode from '../../images/visual-design.png';
+import cart from '../../images/shopping-cart.png'
 const Card = () => {
   return (
     <div className="card">
-      <div className="card-header d-flex flex-row justify-content-center">
-        <div className="card-image-container">
-          <img src={dataset} alt="Card" className="dataset-image" />
-          <div class="button-block">
-            <button class="data-button">dataset</button>
-          </div>
+      <div className="card-body">
+
+
+            <div className="d-flex flex-row">
+
+
+    <div className="d-flex flex-row">
+
+     <div>
+        <div className="dataset-details d-flex flex-column">
+
+            <div>
+            <img src={dataset} alt="Card" className="dataset-image" />
+            </div>
+             <div>
+             <button class="data-button">dataset</button>
+            </div>
+         </div>
+            
+           
+
+           {/* <div className="certified-details d-flex flex-row"> */}
+
+        <div className="d-flex flex-column">
+            <div>
+            <img src={certified} alt="certified-logo" width="40px"height="40px"/>
+            </div>
+            <div>
+             <p>Certified</p>
+            </div>
+
         </div>
-        <div className="card-heading">
-    <div className="heading-box">
-            <h1>Employee Sales Meta Data Information</h1>
+
+
+        <div className="d-flex flex-column"> 
+             {/* <div className="registered details d-flex flex-row"> */}
+            <div>
+            <img src={registered} alt="registered-logo" width="40px"height="40px"/>"
+            </div>
+            <div>
+            <p>Registered</p>
             </div>
         </div>
-    
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">Card Title</h5>
-        <p className="card-text">This is a sample card with some content.</p>
-        <a href="/" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
+
     </div>
-  );
-};
+
+    <div>
+        <div className="node-heading">
+          <h1>Employee Sales Meta Data Information</h1>
+        </div>
+
+    </div>
+</div>  {/*flex-row completed */}
+
+<div className="addtocart-button d-flex flex-column" >
+            <button>Add to Cart   <img src={cart} alt="pinned" width="40px" height="40px"/></button>
+            <button>Add to Saved   <img src={pinned} alt="pinned" width="40px" height="40px"/>   </button>
+            <button>Share   <img src={share} alt="Share" width="40px" height="40px"/>   </button>
+            <button>View Node  <img src={viewnode} alt="Share"width="40px" height="40px"/>   </button> 
+</div>
+
+</div>
+</div>
+</div>
+)}
+
+
+       
 
 export default Card;
