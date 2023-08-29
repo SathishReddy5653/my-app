bbbc53314e659d01dcac45c060f1722e76f74c8f
sathish <sathishreddy2902@gmail.com>
2023-08-29 15:17:21 +0530

    displayed cards side by side

--- src/components/card-component/card.js
+++ src/components/card-component/card.js
@@ -1,96 +1,190 @@
 import React from "react";
 import "../card-component/card.css";
-import dataset from "../../images/dataset.png";
-import certified from "../../images/certified.jpg";
-import registered from "../../images/registered.jpeg";
+// import dataset from "../../images/dataset.png";
+// import certified from "../../images/certified.jpg";
+// import registered from "../../images/registered.jpeg";
 import pinned from "../../images/pin.png";
 import share from "../../images/share.png";
 import viewnode from "../../images/visual-design.png";
 import cart from "../../images/shopping-cart.png";
-const Card = () => {
-  return (
-    <div className="card">
-      <div className="card-body">
-        <div className="d-flex flex-row">
+// const Card = () => {
+//   return (
+//     <div className="card">
+//       <div className="card-body">
+//         <div className="d-flex flex-row">
+//           <div className="d-flex flex-row">
+//             <div>
+//               <div className="dataset-details d-flex flex-column">
+//                 <div>
+//                   <img src={dataset} alt="Card" className="dataset-image" />
+//                 </div>
+//                 <div>
+//                   <button class="data-button">dataset</button>
+//                 </div>
+//               </div>
+
+//               {/* <div className="certified-details d-flex flex-row"> */}
+
+//               <div className="d-flex flex-column">
+//                 <div>
+//                   <img
+//                     src={certified}
+//                     alt="certified-logo"
+//                     width="30px"
+//                     height="30px"
+//                   />
+//                 </div>
+//                 <div>
+//                   <p>Certified</p>
+//                 </div>
+//               </div>
+
+//               <div className="d-flex flex-column">
+//                 {/* <div className="registered details d-flex flex-row"> */}
+//                 <div>
+//                   <img
+//                     src={registered}
+//                     alt="registered-logo"
+//                     width="30px"
+//                     height="30px"
+//                   />
+//                   "
+//                 </div>
+//                 <div>
+//                   <p>Registered</p>
+//                 </div>
+//               </div>
+//               <div>
+//                 <h3> 0% </h3>
+//               </div>
+//             </div>
+
+//             <div>
+//               <div className="node-heading">
+//                 <h1>Employee Sales Meta Data Information</h1>
+//               </div>
+//               <div className=" my-buttons d-flex flex-row">
+//                 <button className="alation">Alation</button>
+//                 <p className="createdby">Curated by Sathish</p>
+//               </div>
+//             </div>
+//           </div>{" "}
+//           {/*flex-row completed */}
+//           <div className="addtocart-button d-flex flex-column">
+//             <button className="cart">
+//               Add to Cart{" "}
+//               <img src={cart} alt="pinned" width="40px" height="40px" />
+//             </button>
+//             <button className="save">
+//               Add to Saved{" "}
+//               <img src={pinned} alt="pinned" width="40px" height="40px" />{" "}
+//             </button>
+//             <button className="share">
+//               Share <img src={share} alt="Share" width="40px" height="40px" />{" "}
+//             </button>
+//             <button className="view">
+//               View Node{" "}
+//               <img src={viewnode} alt="Share" width="40px" height="40px" />{" "}
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+
+const Cards = (p) => {
+
+// const addToCart=()=>{
+
+// }
+    return (
+        
+      <div className="card ">
+        <div className="card-body">
           <div className="d-flex flex-row">
-            <div>
-              <div className="dataset-details d-flex flex-column">
-                <div>
-                  <img src={dataset} alt="Card" className="dataset-image" />
-                </div>
-                <div>
-                  <button class="data-button">dataset</button>
-                </div>
-              </div>
-
-              {/* <div className="certified-details d-flex flex-row"> */}
-
-              <div className="d-flex flex-column">
-                <div>
-                  <img
-                    src={certified}
-                    alt="certified-logo"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div>
-                  <p>Certified</p>
-                </div>
-              </div>
-
-              <div className="d-flex flex-column">
-                {/* <div className="registered details d-flex flex-row"> */}
-                <div>
-                  <img
-                    src={registered}
-                    alt="registered-logo"
-                    width="30px"
-                    height="30px"
-                  />
-                  "
-                </div>
-                <div>
-                  <p>Registered</p>
-                </div>
-              </div>
+            <div className="d-flex flex-row">
               <div>
-                <h3> 0% </h3>
+                <div className="dataset-details d-flex flex-column">
+                  <div>
+                    <img src={p.image} alt="Card" className="dataset-image" />
+                  </div>
+                  <div>
+                    <button class="data-button">{p.buttonName}</button>
+                  </div>
+                </div>
+  
+                {/* <div className="certified-details d-flex flex-row"> */}
+  
+                <div className="d-flex flex-column">
+                  <div>
+                    <img
+                      src={p.certified_logo}
+                      alt="certified-logo"
+                      width="30px"
+                      height="30px"
+                    />
+                  </div>
+                  <div>
+                    <p>{p.certified_text}</p>
+                  </div>
+                </div>
+  
+                <div className="d-flex flex-column">
+                  {/* <div className="registered details d-flex flex-row"> */}
+                  <div>
+                    <img
+                      src={p.register_logo}
+                      alt="registered-logo"
+                      width="30px"
+                      height="30px"
+                    />
+                    "
+                  </div>
+                  <div>
+                    <p>{p.register_text}</p>
+                  </div>
+                </div>
+                <div>
+                  <h3> {p.meta_quality}% </h3>
+                </div>
+              </div>
+  
+              <div>
+                <div className="node-heading">
+                  <h1>{p.node_heading}</h1>
+                </div>
+                <div className=" my-buttons d-flex flex-row">
+                  <button className="alation">{p.button_alation}</button>
+                  <p className="createdby">Curated by {p.name}</p>
+                </div>
               </div>
             </div>
-
-            <div>
-              <div className="node-heading">
-                <h1>Employee Sales Meta Data Information</h1>
-              </div>
-              <div className=" my-buttons d-flex flex-row">
-                <button className="alation">Alation</button>
-                <p className="createdby">Curated by Sathish</p>
-              </div>
+            {/*flex-row completed */}
+            <div className="addtocart-button d-flex flex-column">
+              <button className="cart">
+                Add to Cart
+                <img src={cart} alt="pinned" width="40px" height="40px" />
+              </button>
+              <button className="save">
+                Add to Saved
+                <img src={pinned} alt="pinned" width="40px" height="40px" />{" "}
+              </button>
+              <button className="share">
+                Share <img src={share} alt="Share" width="40px" height="40px" />{" "}
+              </button>
+              <button className="view">
+                View Node
+                <img src={viewnode} alt="Share" width="40px" height="40px" />{" "}
+              </button>
             </div>
-          </div>{" "}
-          {/*flex-row completed */}
-          <div className="addtocart-button d-flex flex-column">
-            <button className="cart">
-              Add to Cart{" "}
-              <img src={cart} alt="pinned" width="40px" height="40px" />
-            </button>
-            <button className="save">
-              Add to Saved{" "}
-              <img src={pinned} alt="pinned" width="40px" height="40px" />{" "}
-            </button>
-            <button className="share">
-              Share <img src={share} alt="Share" width="40px" height="40px" />{" "}
-            </button>
-            <button className="view">
-              View Node{" "}
-              <img src={viewnode} alt="Share" width="40px" height="40px" />{" "}
-            </button>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+  
+    );
+  };
 
-export default Card;
+  export default Cards;
--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -41,25 +41,101 @@
     import Tabs from 'react-bootstrap/Tabs';
     // import "../tabs/tabs.scss";
     import "../tabs/tabs.css";
-import Card from '../card-component/card';
+import Cards from '../card-component/card';
 
     // import Welcome from '../Welcome'
 
+    import dataset from "../../images/dataset.png";
+    import certified from "../../images/certified.jpg";
+    import registered from "../../images/registered.jpeg";
+let cardDetails=[{
+  id:1,
+  image:dataset,
+buttonName:"DataSet",
+certified_logo:certified,
+certified_text:"Certified",
+register_logo:registered,
+register_text:"Registered",
+meta_quality:20 ,
+node_heading:"Employee Salary Details",
+button_alation:"Alation",
+name:"Sathish",
+}
+,{
+  id:2,
+  image:dataset,
+buttonName:"MyDataSet",
+certified_logo:certified,
+certified_text:"Certified",
+register_logo:registered,
+register_text:"Registered",
+meta_quality:50 ,
+node_heading:"Purchase Details",
+button_alation:"Alation",
+name:"Jeevitha",
+},
+{
+  id:3,
+  image:dataset,
+buttonName:"DataSet",
+certified_logo:certified,
+certified_text:"Certified",
+register_logo:registered,
+register_text:"Registered",
+meta_quality:80,
+node_heading:"Sales of the Month",
+button_alation:"Alation",
+name:"Sai Kiran Reddy",
+}
+,
+{
+  id:4,
+  image:dataset,
+buttonName:"DataSet",
+certified_logo:certified,
+certified_text:"Certified",
+register_logo:registered,
+register_text:"Registered",
+meta_quality:90,
+node_heading:"Recordings of July",
+button_alation:"Ranger",
+name:"Saha",
+}
+
+]
+
+
 const TabButton=()=> {
   return(
-    <div >
+    <div justify-content-center>
     <Tabs
     defaultActiveKey="profile"
     id="justify-tab-example"
-    className="mb-3 custom-tab justify-content-center"
+    className="mb-3 custom-tab "
   justify
   >
-    <Tab eventKey="home" title="Home">
-      < Card/>
-    </Tab>
-    <Tab eventKey="profile" title="Profile">
+    
+    <Tab eventKey="profile" title="Collections" >
       Tab content for Profile
     </Tab>
+    <Tab eventKey="home" title="Nodes" className="d-flex flex-row flex-wrap justify-content-start">
+      {
+      cardDetails.map((x)=>{
+        return <Cards key={x.id} image={x.image} 
+        buttonName={x.buttonName} 
+         certified_logo={x.certified_logo}
+         certified_text={x.certified_text}
+         register_logo={x.register_logo}
+         register_text={x.register_text}
+         meta_quality={x.meta_quality}
+         node_heading={x.node_heading}
+         button_alation={x.button_alation}
+         name={x.name} />
+      })
+      }
+
+    </Tab>
+    
     
   </Tabs>
   </div>
